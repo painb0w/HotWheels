42750133fcefd3894e27d4a173d0d95277cd5861
JoePeach762 <123075586+JoePeach762@users.noreply.github.com>
2024-12-13 13:04:20 +0500

    sosal?
    descr

--- parse_data.py
+++ parse_data.py
@@ -1,1 +1,25 @@
-cars = 'тачки'
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import os
+os.system('taskkill /f /im chromedriver.exe')
+os.system('taskkill /f /im chrome.exe')
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+def getItemDivs(url):
+    # Создаем объект ChromeOptions
+    options = Options()
+
+    # Запускаем драйвер с указанными параметрами
+    driver = webdriver.Chrome()
+
+    driver.get(url)
+    print(driver.title)
+    driver.quit()
+    
+
+# Пример использования
+getItemDivs('https://www.yahoo.com')
