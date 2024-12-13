cd079327495ffebedc776962cfe0b9a256daec97
JoePeach762 <123075586+JoePeach762@users.noreply.github.com>
2024-12-13 13:06:05 +0500

    sosal2
    nasral

--- parse_data.py
+++ parse_data.py
@@ -3,8 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import os
-os.system('taskkill /f /im chromedriver.exe')
-os.system('taskkill /f /im chrome.exe')
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
