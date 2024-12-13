747bb561966defc2868e9705b5756d4742d88b1d
JoePeach762 <123075586+JoePeach762@users.noreply.github.com>
2024-12-13 13:14:09 +0500

    Update parse_data.py

--- parse_data.py
+++ parse_data.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import os
 
+#comment
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
