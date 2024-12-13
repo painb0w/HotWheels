4ec2e9072e69beb8d629fc3da0ec31d788f574e8
painb0w <pashayandex8@gmail.com>
2024-12-13 16:56:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import unzip
 source_folder = "C:/University/5 semester/ml projects/final project/Выборки"  # Сюда парсер скачивает выборки
 destination_folder = "C:/Users/Pavel/Documents/GitHub/HotWheels/Выборки"  # Сюда парсер распаковывает архивы из source_folder
 
-unzip.extract_gz_files(source_folder, destination_folder)
 parse_data.get_weather_archive(source_folder)
+unzip.extract_gz_files(source_folder, destination_folder)
